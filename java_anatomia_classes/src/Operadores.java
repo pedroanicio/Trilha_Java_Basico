79275d9d8229aee47f72d74a749d42441eb50456
pedroanicio <pedranx@gmail.com>
2024-04-24 19:17:01 -0300

    Adicao classes de variaveis e metodos

---
+++ java_anatomia_classes/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        //Operador ternario
+
+        int a = 6, b = 6;
+
+        String result = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(result);
+
+        //relacionais
+
+        // ==
+        // !=
+        // >
+        // >=
+        // <
+        // <=
+
+        //logicos
+
+        //&&
+        //||
+    }
+}
--- java_anatomia_classes/src/TiposVariaveis.java
+++ java_anatomia_classes/src/TiposVariaveis.java
@@ -1,7 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        final Double teste = 20.0;
+        double salario = 2500.34;
+        int idade = 32;
+        String nome = "Pedro";
+        final double pi = 3.14;
+        boolean trueFalse = true;
 
-        System.out.println(teste);
+        //lista de idades, por isso o plural
+        int idades[] = new int[4];
     }
 }
