006cb59fe815865003f919f6c788fc813baefde5
pedroanicio <pedranx@gmail.com>
2024-05-16 12:30:47 -0300

    adding oop

---
+++ java_anatomia_classes/src/POO/Pilares/Autodromo.java
@@ -0,0 +1,18 @@
+package POO.Pilares;
+
+public class Autodromo {
+    public static void main(String[] args){
+        Carro Palio = new Carro();
+        Palio.setChassi("354345");
+        Palio.ligar();
+
+        Moto nomemoto = new Moto();
+        nomemoto.setChassi("12344");
+        nomemoto.ligar();
+
+        Veiculo coringa = Palio; //tente trocar para nomemoto
+
+        //polimorfismo
+        coringa.ligar();
+    }
+}
