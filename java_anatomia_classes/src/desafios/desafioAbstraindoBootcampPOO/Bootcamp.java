9c70b2538ed8f54bcbd0f4e82c32d0b54b4f664f
pedroanicio <pedranx@gmail.com>
2024-05-27 23:01:43 -0300

    studying maven

---
+++ java_anatomia_classes/src/desafios/desafioAbstraindoBootcampPOO/Bootcamp.java
@@ -0,0 +1,9 @@
+package desafios.desafioAbstraindoBootcampPOO;
+
+import java.time.LocalDate;
+
+public class Bootcamp {
+    private String nome, descricao;
+    private final LocalDate dataInicial = LocalDate.now(), dataFinal = dataInicial.plusDays(45);
+    //private  Set<Dev>
+}
